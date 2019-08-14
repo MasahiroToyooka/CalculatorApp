bbc50be896df525749c1cb022a0d6c0df0c79e99
MasahiroToyooka <ehara2481@gmail.com>
2019-08-14 21:40:52 +0900

    計算ボタンを押した時のアクション

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -64,7 +64,79 @@ class ViewController: UIViewController {
     @IBAction func actionsButton(_ sender: UIButton) {
         
         
-        
+        // ラベルを書き換える
+        editLabel = true
+        if sender.tag == 10 { // Cが押されたとき
+            // クリアする
+            allClear()
+            //                resultCalculate.text = ""
+            //                result = 0
+            //                previousNumber = 0
+            //                numberOnScreen = 0
+            //                operation = 0
+            //                inValue = false
+            //                editLabel = true
+            //                performingMath = false
+        } else if inValue && sender.tag == 15  {// = が押された時
+            // 演算子を判定して、演算を実行
+            switch operation {
+            case 11:
+                if numberOnScreen != 0 {
+                    result = previousNumber / numberOnScreen
+                } else {
+                    result = Double.infinity
+                }
+            case 12:
+                result = previousNumber * numberOnScreen
+            case 13:
+                result = previousNumber + numberOnScreen
+            case 14:
+                result = previousNumber - numberOnScreen
+            default:
+                break
+            }
+            // 小数点で値を分離
+            let shosu: [String] = String(result).components(separatedBy: ".")
+            
+            if result.isInfinite {
+                resultCalculate.text = "エラー"
+                allClear()
+            } else if shosu[1] == "0" {
+                // 小数点以下が0であるなら
+                resultCalculate.text = String(Int(result))
+                inValue = true
+            }else { // 小数点以下が0でない
+                resultCalculate.text = String(result)
+                inValue = true
+            }
+            
+            // 数値を足せないようにする
+            editLabel = false
+        } else { // +,-,×,÷のいずれかが押されたとき
+            // 画面に表示されている数字を変数に代入
+            if inValue {
+                previousNumber = Double(resultCalculate.text!)!
+                print(previousNumber)
+            }
+            switch sender.tag {
+            case 11:
+                resultCalculate.text = "÷"
+            case 12:
+                resultCalculate.text = "×"
+            case 13:
+                resultCalculate.text = "+"
+            case 14:
+                resultCalculate.text = "-"
+            default:
+                break
+            }
+            inValue = false
+            // 演算子を記憶
+            operation = sender.tag
+            // 計算していい
+            performingMath = true
+        }
+    }
         
     }
 }
